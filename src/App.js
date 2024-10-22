4e6ae7c3245482a7813dab9dd5f3403d3b671ac4
Reetha47 <reethamaria47@gmail.com>
2024-10-22 08:32:17 +0530

    made the app responsive to all devices

--- src/App.js
+++ src/App.js
@@ -16,24 +16,23 @@ function App() {
       <h1 className='text-4xl font-serif text-red-700'>
         Counter App
       </h1>
-      <div className='text-9xl'> {count}</div>
-    <br>
-    </br>
-    <div className='flex flex-row justify-items-center items-center gap-32'>
-    <div  className='text-2xl font-mono'>
-      <button  className=' border-black border-2 text-3xl font-semibold items-center hover:bg-blue-800  bg-blue-600 rounded-sm' onClick={increment}>
+      <div className='text-3xl sm:text-5xl lg:text-9xl font-bold'> {count}</div>
+    
+    <div className='flex flex-row justify-items-center items-center gap-4 sm:gap-6 lg:gap-32'>
+    <div  className='font-mono'>
+      <button  className=' border-black border-2 text:xl sm:text-2xl md:text-3xl font-semibold items-center px-2 hover:bg-blue-800  bg-blue-600 rounded-sm' onClick={increment}>
         Increment
       </button>
     </div>
-    <div className='text-2xl font-mono'>
+    <div className=' font-mono'>
      
       
-      <button className=' border-black border-2 text-3xl font-semibold align-middle w-40 items-center  hover:bg-blue-800 bg-blue-600 rounded-sm' onClick={decrement}>
+      <button className=' border-black border-2 text:xl sm:text-2xl md:text-3xl font-semibold align-middle w-40 items-center  hover:bg-blue-800 bg-blue-600 rounded-sm' onClick={decrement}>
       Decrement
       </button>
     </div>
-    <div className='text-2xl font-mono' >
-      <button className=' border-black border-2 text-3xl font-semibold items-center  hover:bg-blue-800 bg-blue-600 rounded-sm' onClick={reset}>
+    <div className=' font-mono' >
+      <button className=' border-black border-2 text:xl sm:text-2xl md:text-3xl font-semibold items-center px-2 hover:bg-blue-800 bg-blue-600 rounded-sm' onClick={reset}>
         Reset
       </button>
     </div>
